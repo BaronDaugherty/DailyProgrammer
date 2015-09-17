3c65d620c6cac868c506e2bb113076edd6f8cf6b
Baron Daugherty <bd4430@gmail.com>
2015-09-16 22:29:57 -0500

    Bug fix and cleanup
    Fixed the bug... but of course I had the current year adding the leap
    day backwards (28 days for leap year, 29 for not... ugh that was silly).
    Other cleanup and formatting

--- Challenge 11/c11_easy.cpp
+++ Challenge 11/c11_easy.cpp
@@ -3,13 +3,10 @@
 	@author:	Baron Daugherty
 	@date:		2015-09-16
 	@desc:		The program should take three arguments: a day, a month, a year
-                	It will then compute the day of the week that date will fall on.
+                It will then compute the day of the week that date will fall on.
 	@note:		Unlike my python and perl submissions, here I have actually
-			constructed and implemented an algorithm to calculate the
-			day of the week rather than use existing libraries.
-			It has a bug, though... Some dates are spot on while others seem
-			to be +/- a day. I have a pretty good idea I know where it lies,
-			I'll come back to it soon
+				constructed and implemented an algorithm to calculate the
+				day of the week rather than use existing libraries.
 */
 
 /* includes & namespaces */
@@ -34,7 +31,7 @@ int main()
 	uint16_t day;
 	
 	//get the date to check
-	cout << "Year(> 1900): ";
+	cout << "Year(>= 1900): ";
 	cin >> year;
 
 	cout << "Month[1-12]: ";
@@ -68,18 +65,17 @@ uint16_t num_of_years(uint16_t year, uint16_t month, uint16_t day)
 	return year - 1900;
 }	//END num_of_years
 
-uint16_t num_of_leap_years(uint16_t years)
+uint16_t num_of_leap_years(uint16_t year)
 //gives us the number of leap years since 1900
 {
 	//some variables
 	uint16_t total = 0;
-	uint16_t start_year = 1900;
+	uint16_t check_year = 1900;
 
 	//check each year 1900 through the number of years given
-	for (int i = 0; i < years; i++)
+	for (check_year; check_year < year; check_year++)
 	{
-		uint16_t curr_year = start_year + i;
-		if(is_leap_year(curr_year))
+		if(is_leap_year(check_year))
 			total += 1;
 	}
 
@@ -93,14 +89,13 @@ uint16_t num_of_days(uint16_t years, uint16_t leap_years,
 {
 	//set up the number of days in a year / months
 	const int DAYS_IN_YEAR = 365;
-	const int DAYS_IN_LEAP = 366;
-	uint16_t days_in_month[12] = {31, is_leap_year(year) ? 28:29, 31, 30, 31, 
+	uint16_t days_in_month[12] = {31, is_leap_year(year) ? 29:28, 31, 30, 31, 
 									30, 31, 31, 30, 31, 30 , 31};
 	
 	//get the initial days based on whole years
 	uint16_t days = 0;
-	days += (years - leap_years) * DAYS_IN_YEAR;
-	days += leap_years * DAYS_IN_LEAP;
+	days += years * DAYS_IN_YEAR;
+	days += leap_years;
 
 	//add days for each month that's passed
 	for (int i = 0; i < (month-1); i++)
@@ -127,10 +122,10 @@ void calculate(uint16_t year, uint16_t month, uint16_t day)
 
 	//do a bunch of the calculations
 	uint16_t years = num_of_years(year, month, day);
-	uint16_t leap_years = num_of_leap_years(years);
+	uint16_t leap_years = num_of_leap_years(year);
 	uint16_t days = num_of_days(years, leap_years, year, month, day);
 
 	//output, calls the day_of_week function to get the answer
 	cout << "\n" << year << "-" << month << "-" << day
-		<< " is (probably) a " << days_of_week[day_of_week(days)] << "\n\n";
+		<< " is a " << days_of_week[day_of_week(days)] << "\n\n";
 }	//END calculate
